d56ce7ff06c06777698a742e750b47e3a54cd5bc
bulikd105 <bulikd105@gmail.com>
2016-10-06 14:54:00 +0200

    Reuse kodu z lab 4, do podłączania klientów na serwer

--- src/pai_projekt/Game.java
+++ src/pai_projekt/Game.java
@@ -1,5 +1,9 @@
 package pai_projekt;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
 /*
  * Klasa Game
  * Odpowiedzialna za uruchomienie serwera, oraz pod³¹czanie nowych graczy do niego
@@ -7,8 +11,29 @@ package pai_projekt;
 
 public class Game 
 {
-	public static void main(String[] args) 
+	// Lista do przechowywania graczy
+	public static ArrayList<Player> playerList = new ArrayList<Player>();
+	
+	public static void main(String[] args) throws IOException 
 	{
+		int serverPort = 8060;
 		
+		ServerSocket serverSocket = new ServerSocket(serverPort);
+		
+		String playerName = "Player "; 
+		int i = 1;
+		
+		try
+		{
+			while(true)
+			{
+				Thread playerThread = new Thread(new Server(serverSocket.accept(), playerName + i++, playerList));
+				playerThread.start();
+			}
+		}
+		finally
+		{
+			serverSocket.close();
+		}
 	}
 }
--- src/pai_projekt/Server.java
+++ src/pai_projekt/Server.java
@@ -1,10 +1,25 @@
 package pai_projekt;
 
+import java.net.Socket;
+import java.util.ArrayList;
+
 /*
  * Klasa Server
  * Posiada metody odpowiedzialne za zarz¹dzanie graczami i obs³ugê gry.
  */
-public class Server 
+public class Server implements Runnable 
 {
+
+	public Server(Socket accept, String string, ArrayList<Player> playerList) 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 }
