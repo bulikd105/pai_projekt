6249b8edabcc3ff66f4d41b4b2dbcdab1b684660
bulikd105 <bulikd105@gmail.com>
2016-11-04 11:31:48 +0100

    Dodanie elementów w klasie Player

--- src/pai_projekt/Player.java
+++ src/pai_projekt/Player.java
@@ -7,8 +7,39 @@ package pai_projekt;
  */
 public class Player 
 {
-	public static void main(String[] args) 
+	private int playerId;
+	private int playerScore;
+	private String playerName;
+	
+	public Player(int playerId, String playerName)
 	{
-		
+		this.playerId = playerId;
+		this.playerName = playerName;
 	}
+
+	public int getPlayerId() 
+	{
+		return playerId;
+	}
+
+	public int getPlayerScore() 
+	{
+		return playerScore;
+	}
+
+	public String getPlayerName() 
+	{
+		return playerName;
+	}
+
+	public void setPlayerScore(int playerScore) 
+	{
+		this.playerScore = playerScore;
+	}
+
+	public void setPlayerName(String playerName) 
+	{
+		this.playerName = playerName;
+	}
+	
 }
