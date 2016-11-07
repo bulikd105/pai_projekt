8cdd44ba7476764dfa7994d484cf244950d1450d
bulikd105 <bulikd105@gmail.com>
2016-11-07 14:48:55 +0100

    Player. Dodanie dodatkwoych pól z flagami

--- src/pai_projekt/Player.java
+++ src/pai_projekt/Player.java
@@ -7,15 +7,26 @@ package pai_projekt;
  */
 public class Player 
 {
-	private int playerId;
-	private int playerScore;
-	private String playerName;
+	private int playerId;			// unikalny klucz ka¿dego gracza
+	private int playerScore; 		// Wynik danego gracza 
+	private String playerName; 		// Nazwa gracza, mo¿liwa do zmiany
+	private boolean isNewPlayer; 	// Flaga 1, czy gracz dopiero do³¹czy³
+	private boolean isGuessing; 	// Flaga 2, do rozró¿niania graczy od osoby wymyœlaj¹cej has³o
+	
+	/*
+	 * Flag1 = true && Flag2 = true 	- Cannot happen
+	 * Flag1 = true && Flag2 = false 	- When new player is added
+	 * Flag1 = false && Flag2 = false 	- When player is giving password
+	 * Flag1 = false && Flag2 = true 	- When player joined the game
+	 * */
 	
 	public Player(int playerId, String playerName)
 	{
 		this.playerId = playerId;
 		this.playerName = playerName;
 		this.playerScore = 0;
+		this.isNewPlayer = true;
+		this.isGuessing = false;
 	}
 
 	public int getPlayerId() 
@@ -31,6 +42,26 @@ public class Player
 	public String getPlayerName() 
 	{
 		return playerName;
+	}
+
+	public boolean isGuessing() 
+	{
+		return isGuessing;
+	}
+	
+	public boolean isNewPlayer() 
+	{
+		return isNewPlayer;
+	}
+
+	public void setNewPlayer(boolean isNewPlayer) 
+	{
+		this.isNewPlayer = isNewPlayer;
+	}
+
+	public void setGuessing(boolean isGuessing) 
+	{
+		this.isGuessing = isGuessing;
 	}
 
 	public void setPlayerScore(int playerScore) 
